9ec3bcf6f6478ef48a682c39cf6899991cf9544c
Sudha Subramaniam <s.sudhacse08@gmail.com>
2019-05-15 22:29:46 +0530

    Add files via upload

---
+++ build/precache-manifest.9582c39f6486408d549714dc80d8635c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21bc697266bab124577533a3b9f51011",
+    "url": "/goslowwhoa/index.html"
+  },
+  {
+    "revision": "1c0a5767ec7177398855",
+    "url": "/goslowwhoa/static/css/main.c66f0261.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/goslowwhoa/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "1c0a5767ec7177398855",
+    "url": "/goslowwhoa/static/js/main.59a3f9dc.chunk.js"
+  },
+  {
+    "revision": "0a6db90212beae8d3370",
+    "url": "/goslowwhoa/static/js/runtime~main.37e6213b.js"
+  },
+  {
+    "revision": "0b89fbafade0f864977a8543430c0d07",
+    "url": "/goslowwhoa/static/media/body-background.0b89fbaf.jpg"
+  }
+]);
